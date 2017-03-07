bf3758a986456d1bdd1f9dc8df9510aed39a5a8b
TEbenhoech <tebenhoehc@csumb.edu>
2017-03-07 10:34:24 -0800

    hand recognition

--- getVideo.py
+++ getVideo.py
@@ -3,8 +3,11 @@ import cv2
 video = cv2.VideoCapture(0)
 while(True):
     ret, frame = video.read()
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',frame)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    ret,thresh1 = cv2.threshold(blur,250,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    cv2.imshow('frame',gray)
+    cv2.imshow('binary',thresh1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 # When everything done, release the capture
