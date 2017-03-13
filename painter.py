7f560d9c10e69ca538bc2ff4d9cde29b839ac8e5
bigbrotherx52 <harlantc@gmail.com>
2017-03-12 20:47:03 -0700

    new painting code
    painter takes mouse x,y cords and paints them.

---
+++ painter.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+video = cv2.VideoCapture(0)
+##Code based off code from opencv doc: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_mouse_handling/py_mouse_handling.html
+ret, frame = video.read()
+height, width = frame.shape[:2]
+drawing = False # true if mouse is pressed
+mode = False # if True, draw rectangle. Press 'm' to toggle to curve
+ix,iy = -1,-1
+
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    global ix,iy,drawing,mode
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            if mode == True:
+                cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            else:
+                cv2.circle(img,(x,y),5,(0,0,255),-1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        if mode == True:
+            cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+        else:
+            cv2.circle(img,(x,y),5,(0,0,255),-1)
+            
+            
+img = np.zeros((height,width,3), np.uint8)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_circle)
+
+while(1):
+    ret, frame = video.read()
+    
+    both = cv2.add(frame, img)
+    
+    cv2.imshow('image',both)
+    #cv2.imshow('image',img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    if k == ord('c'):
+        img = np.zeros((height,width,3), np.uint8)
+    elif k == 27:
+        break
+
+cv2.destroyAllWindows()
