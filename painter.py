1ffccbb2f569d465fb9378928633c27b7b3aeec0
bigbrotherx52 <harlantc@gmail.com>
2017-03-14 11:06:01 -0700

    painter update
    added a draw from the position of tha fist

--- painter.py
+++ painter.py
@@ -3,6 +3,7 @@ import numpy as np
 video = cv2.VideoCapture(0)
 ##Code based off code from opencv doc: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_mouse_handling/py_mouse_handling.html
 ret, frame = video.read()
+hand_cascade = cv2.CascadeClassifier('fist.xml')
 height, width = frame.shape[:2]
 drawing = False # true if mouse is pressed
 mode = False # if True, draw rectangle. Press 'm' to toggle to curve
@@ -36,18 +37,40 @@ cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
 
 while(1):
+   
+    posX = 0
+    posY = 0
     ret, frame = video.read()
     
     both = cv2.add(frame, img)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    hand = hand_cascade.detectMultiScale(gray,1.3,4)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
     
-    cv2.imshow('image',both)
+    
+    for(x,y,w,h) in hand:
+        #draws a rectangle around the 
+        cv2.rectangle(frame,(x,y),(x+w, y+h),(0,255,0),2)
+        posX = x+(w/2)
+        posY = y+(h/2)
+    
+    
+
+    
+    cv2.circle(img,(posX,posY),10,(0,0,255),-1)
     #cv2.imshow('image',img)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('m'):
         mode = not mode
     if k == ord('c'):
-        img = np.zeros((height,width,3), np.uint8)
+        img = np.zeros((height,width,3), np.uint8)    
     elif k == 27:
         break
+    
+    both = cv2.add(frame, img)
+    
+
+    
+    cv2.imshow('image',both)
 
 cv2.destroyAllWindows()
