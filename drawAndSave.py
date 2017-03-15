1a8d217d5665c7634dce787c2558cd34e149c56e
Samuel Valdez <savaldez@csumb.edu>
2017-03-14 17:03:52 -0700

    drawAndSave.py
    Implemented the save feature and clear feature

--- drawAndSave.py
+++ drawAndSave.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 video = cv2.VideoCapture(0)
 ##Code based off code from opencv doc: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_mouse_handling/py_mouse_handling.html
 ret, frame = video.read()
@@ -50,19 +51,40 @@ while(1):
     
     blur = cv2.GaussianBlur(gray,(5,5),0)
     
-    
+    b= 0
+    g = 0
+    r = 255
     for(x,y,w,h) in hand:
         #draws a rectangle around the 
-        cv2.rectangle(frame,(x,y),(x+w, y+h),(0,255,0),2)
+        cv2.rectangle(frame,(x,y),(x+w, y+h),(b,g,r),2)
         posX = x+(w/2)
         posY = y+(h/2)
     
+    eraser = (600,220, 0)
+    collisionX = posX - eraser[0]
+    collisionY = posY - eraser[1]
+    eraserCollision = math.sqrt((collisionX * collisionX) + (collisionY * collisionY))
+    eraseFlag = True
+    if eraserCollision < 35:#you can change this if you like to make the radius wider but 30 works well
+        eraseFlag = False
+    
+    save = (310,50,0)
+    colX = posX - save[0]
+    colY = posY - save[1]
+    saveCollision = math.sqrt((colX * colX)+ (colY * colY))
+    saveFlag = True
+    if saveCollision < 30:
+        saveFlag = False
     
 
     
     cv2.circle(img,(posX,posY),10,(0,0,255),-1)
     #cv2.imshow('image',img)
     k = cv2.waitKey(1) & 0xFF
+    if(eraseFlag == False):
+        img = np.zeros((height,width,3), np.uint8) 
+    if(saveFlag == False):
+        cv2.imwrite('test.jpg',both)
     if k == ord('m'):
         mode = not mode
     if k == ord('s'):
